ea906424bb7bf7148deb07a05ea286cc43c1ceac
Alan Wong <captainalan@gmail.com>
2019-11-08 17:35:46 -0800

    Ignoring test files, some code cleanup

--- find-and-replace.go
+++ find-and-replace.go
@@ -1,6 +1,6 @@
 // Find and replace script
 // =======================
-// 
+//
 // Taking some of the logic from the link below...
 // http://xahlee.info/python/findreplace_regex.html
 // ...and translating everything into Golang
@@ -9,17 +9,19 @@
 package main
 
 import (
-	"fmt"
 	"container/list"
- 	"io/ioutil"
- 	"path/filepath"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"regexp"
 )
 
 // Declare an array of filenames
-var file_list list.List 
+var file_list list.List
+
 // Input directory
 var input_dir string = filepath.Join("foo", "bar")
+
 // Extension(s) of files to do find and replace in
 var file_extension_regex = regexp.MustCompile(`\.txt$`)
 
@@ -34,16 +36,16 @@ var find_replace_list list.List
 // is replace string.
 type FindAndReplacePair struct {
 	RegularExpression string // gets compiled later
-	Replacement string
+	Replacement       string
 }
 
 func main() {
 	// Define find and replace pairs (here, I just do one)
-	FooToBar := FindAndReplacePair{ "foo", "bar" }
-	BarToBaz := FindAndReplacePair{ "bar", "baz" }
-	BazToFoo := FindAndReplacePair{ "baz", "foo" }
+	FooToBar := FindAndReplacePair{"foo", "bar"}
+	BarToBaz := FindAndReplacePair{"bar", "baz"}
+	BazToFoo := FindAndReplacePair{"baz", "foo"}
 
-	find_replace_list.PushFront(FooToBar)
+	find_replace_list.PushBack(FooToBar)
 	find_replace_list.PushBack(BarToBaz)
 	find_replace_list.PushBack(BazToFoo)
 	// Push more things to find_replace_list as needed
@@ -55,10 +57,13 @@ func main() {
 	fmt.Println(min_level, max_level, input_dir, file_extension_regex, file_list)
 
 	// reads input_dir top, specified in top level code
-
 	// TODO remove!! temporary, for testing
 	new_path := filepath.Join(input_dir, "thing.txt")
-	replace_string_in_file(new_path) 
+	replace_string_in_file(new_path)
+
+	fmt.Println("Time to replace some stuff. The time is...")
+	// TODO insert date/time
+	// TODO iterate through files in directory to specified depth
 }
 
 // Replaces all strings by regex in find_replace_list at fpath
@@ -67,18 +72,16 @@ func replace_string_in_file(fpath string) {
 	// Note: here, we read an *entire* file, rather than going
 	// line by line
 	input_file, err := ioutil.ReadFile(fpath) // For read access.
-	input_text :=  string(input_file)
+	input_text := string(input_file)
 
 	if err != nil {
 		panic(err) // blow up n quit
 	}
 
 	// iterate through find_replace_list
-	for current := find_replace_list.Front();
-	current != nil;
-	current = current.Next() {
+	for current := find_replace_list.Front(); current != nil; current = current.Next() {
 		// Type assertion used here!
-		find_and_replace_pair := current.Value.(FindAndReplacePair) 
+		find_and_replace_pair := current.Value.(FindAndReplacePair)
 
 		// Compile regexp to use for find and replace task
 		current_regexp, _ := regexp.CompilePOSIX(
@@ -89,38 +92,21 @@ func replace_string_in_file(fpath string) {
 			find_and_replace_pair.Replacement)
 
 		// Write the result to file
-		// TODO find what appropriate permissions should be 
+		// TODO find what appropriate permissions should be
 		ioutil.WriteFile(fpath, []byte(new_text), 0755)
 
+		// Update input text to the changes just written to file
+		input_text = new_text
+
 		// TODO Write some more useful output
-		fmt.Println("Text with replacements: ", new_text)
+		fmt.Printf("Text with replacements (%v -> %v):\n%v\n",
+			string(find_and_replace_pair.RegularExpression),
+			string(find_and_replace_pair.Replacement),
+			new_text)
 	}
 }
 
 /*
-print(datetime.datetime.now())
-print("Input Dir:", input_dir)
-for x in find_replace_list:
-   print("Find regex:「{}」".format(x[0]))
-   print("Replace pattern:「{}」".format(x[1]))
-   print("\n")
-
-if (len(file_list) != 0):
-   for ff in file_list: replace_string_in_file(os.path.normpath(ff) )
-else:
-    for dirPath, subdirList, fileList in os.walk(input_dir):
-        curDirLevel = dirPath.count( os.sep) - input_dir.count( os.sep)
-        curFileLevel = curDirLevel + 1
-        if min_level <= curFileLevel <= max_level:
-            for fName in fileList:
-                if (re.search(file_extension_regex, fName, re.U)):
-                    replace_string_in_file(dirPath + os.sep + fName)
-
-print("Done.")
-*/
-
-
-/* 
 Questions/learning topics
 -------------------------
 array vs slices vs lists---it seems slices are most favored
