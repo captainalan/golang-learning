385406ec248e84522566d81e74acabb09ae7dab0
Alan Wong <captainalan@gmail.com>
2020-04-26 17:58:12 -0700

    Improved solution for rot13 reader

--- exercise-rot-reader.go
+++ exercise-rot-reader.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -11,10 +10,7 @@ type rot13Reader struct {
 	r io.Reader
 }
 
-func (z *rot13Reader) Read(p []byte) (n int, err error) {
-	// Read bytes into p
-	n, err = z.r.Read(p) // TODO figure out how to get this to NOT print
-
+func rot13(b byte) byte {
 	// Decode the cipher!
 	// Cipher only applies to [A-Za-z]
 	const A_ascii byte = 65
@@ -22,22 +18,27 @@ func (z *rot13Reader) Read(p []byte) (n int, err error) {
 	const a_ascii byte = 97
 	const z_ascii byte = 122
 
+	if b >= A_ascii && b <= Z_ascii {
+		// Uppercase letters
+		return (((b - A_ascii) + 13) % 26) + A_ascii
+
+	} else if b >= a_ascii && b <= z_ascii {
+		// Lowercase letters
+		return (((b - a_ascii) + 13) % 26) + a_ascii
+
+	} else {
+		// This case will apply to spaces, punctuation, etc.
+		return b // Just return argument
+	}
+}
+
+func (z *rot13Reader) Read(p []byte) (n int, err error) {
+	// Read bytes into p
+	n, err = z.r.Read(p)
+
 	for i := 0; i < n; i++ {
-		var current byte = p[i] // Current letter (type int)
-		if current >= A_ascii && current <= Z_ascii {
-			// Uppercase letters
-			rot13 := (((p[i] - A_ascii) + 13) % 26) + A_ascii
-			fmt.Printf("%c", rot13)
+		p[i] = rot13(p[i])
 
-		} else if current >= a_ascii && current <= z_ascii {
-			// Lowercase letters
-			rot13 := (((p[i] - a_ascii) + 13) % 26) + a_ascii
-			fmt.Printf("%c", rot13)
-
-		} else {
-			// This case will apply to spaces, punctuation, etc.
-			fmt.Printf("%c", p[i])
-		}
 	}
 	return n, err
 }
